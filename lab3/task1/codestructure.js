8caa25ed0367fa71d088621bfa1868e81af5dbd9
sakenaskar <156654370+sakenaskar@users.noreply.github.com>
2025-02-09 22:17:41 +0500

    Add files via upload

---
+++ lab3/task1/codestructure.js
@@ -0,0 +1,31 @@
+alert('Hello'); alert('World');
+
+alert('Hello');
+alert('World');
+
+alert('Hello')
+alert('World')
+
+alert(3 +
+    1
+    + 2);
+
+alert("Hello");
+
+[1, 2].forEach(alert);
+
+alert("Hello")
+
+[1, 2].forEach(alert);
+
+alert("Hello")[1, 2].forEach(alert);
+
+// This comment follows the statement
+
+/* An example with two messages.
+This is a multiline comment.
+*/
+
+/* Commenting out the code
+alert('Hello');
+*/
---
+++ lab3/task1/themodernmode.js
@@ -0,0 +1,13 @@
+"use strict";
+
+alert("Hello")
+
+'use strict'; 
+//  ...your code
+<Enter to run></Enter>
+
+(function() {
+    'use strict';
+  
+    // ...your code here...
+  })()
